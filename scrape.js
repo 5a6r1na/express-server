014c85cce6b6720724f6ab249c1dd081f93890b1
Sabrina <chiawenli.1219@gmail.com>
2024-05-02 19:48:24 +0800

    [MODIFY] move trigger point to api

--- scrape.js
+++ scrape.js
@@ -31,22 +31,4 @@ async function scrapeWebsite(url) {
   }
 }
 
-const requestData = {
-  depa: "TPE",
-  dest: "OKA",
-  OUT_DATE: "01%2F05%2F2024",
-  IN_DATE: "30%2F05%2F2024",
-  adults: "1",
-};
-const websiteUrl = `https://flight.eztravel.com.tw/tickets-roundtrip-${requestData.depa}-${requestData.dest}/?outbounddate=${requestData.OUT_DATE}&inbounddate=${requestData.IN_DATE}&dport=&aport=&adults=${requestData.adults}&children=0&infants=0&direct=true&cabintype=tourist&airline=&searchbox=t`;
-console.log(websiteUrl);
-scrapeWebsite(websiteUrl)
-  .then((flightData) => {
-    console.log("Flight data:");
-    console.log(flightData);
-  })
-  .catch((error) => {
-    console.error("Error during scraping:", error);
-  });
-
 module.exports = { scrapeWebsite };
